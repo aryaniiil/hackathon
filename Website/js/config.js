29acdc0ca34bb87f91072c022637454cda1f54a2
aryaniiil <aryuggnoob@gmail.com>
2026-08-29 06:59:43 +0530

    backend changes

--- Website/js/config.js
+++ Website/js/config.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://127.0.0.1:8000"; 
+const BACKEND_URL = "http://skillyhackathon.duckdns.org/"; 
 (function() {
   let base = BACKEND_URL || window.API_BASE || "";
   if (!base) {
